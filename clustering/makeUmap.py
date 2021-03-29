2cd6833a1bd43beda8c58fffd13963e43f798872
FlorenceJiang <40256822+FlorenceJiang@users.noreply.github.com>
2021-03-29 10:39:09 -0700

    update
    create clustering files

---
+++ clustering/makeUmap.py
@@ -0,0 +1,100 @@
+import glob
+from collections import defaultdict
+import numpy as np
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %matplotlib inline
+import umap
+import hdbscan
+import sklearn.cluster as cluster
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
+import seaborn as sns
+from tqdm import tqdm
+from mpl_toolkits.mplot3d import Axes3D
+import argparse
+import os
+
+
+def load_data(DATA_DIR):
+    data_dic = defaultdict()
+    for level in range(2,7):
+        file_lst = glob.glob(DATA_DIR+"*p"+str(level)+"*.npy")
+        data_dic[level] = np.vstack([np.load(f) for f in file_lst])
+        print('Loading data...')
+        print('p'+str(level),data_dic[level].shape)
+    return data_dic
+
+
+def umap_embedding(real_data, syn_data, num_components=3, isReal2Syn=True, randn_state=42):
+    if isReal2Syn:
+        base, map_ = real_data, syn_data
+    else:
+        base, map_ = syn_data, real_data
+    umap_model = umap.UMAP(random_state=randn_state, n_components=num_components).fit(base)
+    base_emb = umap_model.transform(base)
+    map_emb = umap_model.transform(map_)
+    if isReal2Syn:
+        real_emb, syn_emb = base_emb, map_emb
+    else:
+        real_emb, syn_emb = map_emb, base_emb
+    return real_emb, syn_emb
+
+
+def plot_2d(real_emb, syn_emb, img_out_dir, isReal2Syn, level):
+    plt.scatter(real_emb[:,0], real_emb[:,1], color='r', label='real')
+    plt.scatter(syn_emb[:,0], syn_emb[:,1], color='g', label='synthetic')
+    plt.legend()
+    LABEL = ' (Synthetic mapped to Real latent space)' if isReal2Syn else ' (Real mapped to Synthetic latent space)'
+    plt.title('Xview UMAP visualization of avg feature map: p'+str(level)+LABEL)
+    plt.xlabel('latent_dim1')
+    plt.ylabel('latent_dim2')
+    # save visual plot
+    f_name = '2d_UMAP_p'+str(level)+LABEL+'.png'
+    plt.savefig(img_out_dir+f_name)
+    print('UMAP vis saved at: ', img_out_dir+f_name)
+
+
+def plot_3d(real_emb, syn_emb, img_out_dir, isReal2Syn, level):
+    fig = plt.figure(figsize=(15, 12))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    ax.scatter3D(real_emb[:,0], real_emb[:,1], real_emb[:,2], marker='x', c='red', label='Real');
+    ax.scatter3D(syn_emb[:,0], syn_emb[:,1], syn_emb[:,2], marker='o', c='green', label='Syn');
+    plt.legend()
+    LABEL = ' (Synthetic mapped to Real latent space)' if isReal2Syn else ' (Real mapped to Synthetic latent space)'
+    print(LABEL)
+    plt.title('Xview 3D UMAP of avg feature map: P'+str(level)+LABEL)
+    plt.xlabel('latent_dim1')
+    plt.ylabel('latent_dim2')
+    plt.ylabel('latent_dim3')
+    out_dir = img_out_dir+'3d_UMAP_p'+str(level)+LABEL+'.png'
+    plt.savefig(out_dir)
+    print('UMAP vis saved at: ', out_dir)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Create UMAP visualization maps')
+    parser.add_argument('--output_dir', help='Output images directory', default='/content/drive/MyDrive/111 Rendered.ai/xview/clustering/training3000iters/')
+    parser.add_argument('--n_samples', help='Number of samples', default=100, type=int)
+    parser.add_argument('--data_dir', help='Data directory', default='/content/drive/MyDrive/111 Rendered.ai/xview/clustering/training3000iters/')
+    args = parser.parse_args()
+    
+    OUT_DIR = args.output_dir
+    N_SAMPLES = args.n_samples
+    DATA_DIR = args.data_dir
+
+    sns.set(style='white', rc={'figure.figsize':(10,8)})
+
+    data_dic = load_data(DATA_DIR)
+
+    for LEVEL in range(2, 7):
+        real_data = data_dic[LEVEL].copy()[:N_SAMPLES]
+        syn_data = data_dic[LEVEL].copy()[N_SAMPLES:]
+        for isReal2Syn in [True, False]:
+            real_emb, syn_emb = umap_embedding(real_data, syn_data, num_components=3, isReal2Syn=isReal2Syn)
+            img_out_dir = OUT_DIR+'visualization/'
+            os.makedirs(img_out_dir, exist_ok=True)
+            plot_3d(real_emb, syn_emb, img_out_dir, isReal2Syn=isReal2Syn, level=LEVEL)
---
+++ clustering/predict_backbone.py
@@ -0,0 +1,123 @@
+# Make sure you installed all necessary libs before running this script.
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+# Import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.modeling import build_model
+from detectron2.checkpoint import DetectionCheckpointer
+# Import some common libraries
+import json
+import pandas as pd
+import math
+from tqdm import tqdm 
+import cv2
+from google.colab.patches import cv2_imshow
+import numpy as np
+import os
+import torch 
+import torchvision
+import torch.nn as nn 
+from IPython.display import Image 
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import random
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+import PIL
+import requests
+from sklearn import decomposition    
+from sklearn.preprocessing import MinMaxScaler
+from tqdm import tqdm_notebook
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from matplotlib.pyplot import imshow
+from PIL import Image
+import IPython
+
+
+def cv2_imshow(img):
+    img = img[:,:,[2,1,0]]
+    img = Image.fromarray(img)
+    plt.figure(figsize=(20, 20))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+
+def normalize(img_collection):
+    data = torch.Tensor(img_collection)
+    # norm_data = (data - data.numpy().mean())/data.numpy().std()
+    return data
+
+
+def inference(data_syn, data_type, batch_size=1, img_size=512, output_dir='/content/drive/MyDrive/111 Rendered.ai/xview/clustering/training3000iters/'):
+    dataloader = DataLoader(data_syn, batch_size=1,
+                            shuffle=False, num_workers=0)
+    # Create feature maps for each level
+    for level in range(2,7):
+        real_ys = []
+        torch.cuda.empty_cache()
+        print('This is level:', level)
+        for batch_id, images in tqdm_notebook(enumerate(dataloader)):
+            torch.cuda.empty_cache()
+            images = images.cuda()
+            x_real = images.reshape((batch_size, 3, img_size, img_size))
+            y_real = model.backbone(x_real)['p'+str(level)].sum(dim=1).reshape(batch_size, -1).cpu().detach()
+            real_ys.append(y_real)
+        y_real = torch.cat(real_ys, dim=0)
+        print('p'+str(level), y_real.shape)
+        
+        # Save output
+        data = y_real.numpy()
+        print(data.shape)
+        os.makedirs(output_dir, exist_ok=True)
+        np.save(output_dir+'pretrained_detectron2_p'+str(level)+'_mean_'+data_type, data)
+        print('Done! level:',level)
+
+
+if __name__ == '__main__':
+        
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    seed = 12345
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+    # Load data
+    OUTPUT_DIR = '/content/drive/MyDrive/111 Rendered.ai/xview/clustering/' # change your dir here
+    real_images = np.load(OUTPUT_DIR+'real_images.npy')
+    syn_images = np.load(OUTPUT_DIR+'syn_images.npy')
+
+    # Normalize data
+    data_real = normalize(real_images)
+    data_syn = normalize(syn_images)
+    print(data_real.dtype, data_real.shape, data_syn.dtype, data_syn.shape)
+
+    # Check images
+    # plt.imshow(data_real[0].numpy().astype(np.int8));plt.show();
+    # plt.imshow(data_syn[0].numpy().astype(np.int8));plt.show();
+
+    # Load pretrained model
+    model_weight_dir = 'model_final.pth'    # change your dir here
+    yaml_dir = 'RCNN_Baseline.yaml'         # change your dir here
+    output_dir = '/content/drive/My Drive/111 Rendered.ai/xview/detectron2_models/'     # change your dir here
+    cfg = get_cfg()
+    cfg.merge_from_file(output_dir+yaml_dir)
+    cfg.MODEL.WEIGHTS = output_dir+model_weight_dir
+    cfg.OUTPUT_DIR = output_dir
+    model = build_model(cfg)  # returns a torch.nn.Module
+    checkpointer = DetectionCheckpointer(model).load(output_dir+model_weight_dir)  # load a file, usually from cfg.MODEL.WEIGHTS
+
+    # Inference 
+    model.eval()
+    print('Inference on real data...')
+    inference(data_real, 'real')
+    print('Inference on synthetic data...')
+    inference(data_syn, 'syn')
